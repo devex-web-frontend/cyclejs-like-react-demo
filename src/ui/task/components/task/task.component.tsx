af4a0c8e6729c4012540ea398ff29055f4ba6b71
Kirill Agalakov <agalakov@devexperts.com>
2019-06-10 17:42:52 +0300

    local title state

--- src/ui/task/components/task/task.component.tsx
+++ src/ui/task/components/task/task.component.tsx
@@ -1,11 +1,12 @@
-import { K, Streamify, reduce, TargetKeyboardEvent, createHandler } from '../../../../utils';
+import { K, Streamify, reduce, TargetKeyboardEvent, createHandler, createValue } from '../../../../utils';
 import * as React from 'react';
 import cx from 'classnames';
 import { ChangeEvent, createRef, FocusEvent, MouseEvent } from 'react';
-import { compose, constVoid } from 'fp-ts/lib/function';
+import { constVoid } from 'fp-ts/lib/function';
 import { Lens } from 'monocle-ts';
-import { Stream } from 'xstream';
+import xs from 'xstream';
 import dropRepeats from 'xstream/extra/dropRepeats';
+import sampleCombine from 'xstream/extra/sampleCombine';
 
 const ESC_KEY = 27;
 const ENTER_KEY = 13;
@@ -31,8 +32,11 @@ export const Task = (props: Streamify<Props>) => {
 	const [handleTitleDoubleClick, titleDoubleClick] = createHandler<MouseEvent<HTMLLabelElement>>();
 	const [handleEditKeyUp, editKeyUpEvent] = createHandler<TargetKeyboardEvent<HTMLInputElement>>();
 	const [handleEditBlur, editBlurEvent] = createHandler<FocusEvent<HTMLInputElement>>();
+	const [handleEditChange, editChangeEvent] = createHandler<ChangeEvent<HTMLInputElement>>();
 
-	const vdom = K(props.value, ({ title, completed, editing }) => {
+	const title = xs.merge(K(editChangeEvent, e => e.target.value), K(props.value, value => value.title));
+
+	const vdom = K(props.value, title, ({ completed, editing }, title) => {
 		const todoRootClassName = cx('todoRoot', {
 			completed,
 			editing,
@@ -46,6 +50,9 @@ export const Task = (props: Streamify<Props>) => {
 					<button className={'destroy'} onClick={handleDestroyClick} />
 				</div>
 				<input
+					value={title}
+					onChange={handleEditChange}
+					autoFocus={editing}
 					type="text"
 					className={'edit'}
 					ref={editInputRef}
@@ -56,25 +63,25 @@ export const Task = (props: Streamify<Props>) => {
 		);
 	});
 
-	const destroy = destroyClickEvent.map(constVoid);
+	const setEditingTrue = editingLens.set(true);
+	const setEditingFalse = editingLens.set(false);
 
-	const value: Stream<TaskValue> = reduce(
+	const destroy = destroyClickEvent.map(constVoid);
+	const enterKeyUp = editKeyUpEvent.filter(e => e.keyCode === ENTER_KEY);
+
+	const value = reduce(
 		props.value,
-		titleDoubleClick.mapTo(editingLens.set(true)),
-		editKeyUpEvent.filter(e => e.keyCode === ESC_KEY).mapTo(editingLens.set(true)),
+		titleDoubleClick.mapTo(setEditingTrue),
+		editKeyUpEvent.filter(e => e.keyCode === ESC_KEY).mapTo(setEditingTrue),
 		toggleChangeEvent
 			.map(e => e.target.checked)
 			.compose(dropRepeats())
 			.map(completedLens.set),
-		Stream.merge(editKeyUpEvent.filter(e => e.keyCode === ENTER_KEY), editBlurEvent)
-			.map(e => e.target.value)
-			.compose(dropRepeats())
-			.map(value =>
-				compose(
-					editingLens.set(false),
-					titleLens.set(value),
-				),
-			),
+		xs.merge(enterKeyUp, editBlurEvent).mapTo(setEditingFalse),
+		xs
+			.merge(enterKeyUp, editBlurEvent)
+			.compose(sampleCombine(title))
+			.map(([_, n]) => titleLens.set(n)),
 	);
 
 	return { vdom, value, destroy };
